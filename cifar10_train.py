82aa7698c916460c5102275276af25e063a212a0
EvgenyKashin <kashinevge@gmail.com>
2019-05-25 15:08:37 +0300

    Change scheduler to Multistep

--- cifar10_train.py
+++ cifar10_train.py
@@ -1,17 +1,18 @@
 import argparse
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import MultiStepLR
+import torch.optim.lr_scheduler
 from back import Bone, utils
 from datasets import cifar10
-from models.resnet import resnet20
-from models.custom_resnet import se_resnet20, srm_resnet20
+from models.resnet import resnet32
+from models.custom_resnet import se_resnet32, srm_resnet32
 
 data_dir = 'cifar10'
 model_names = ['resnet', 'senet', 'srmnet']
 num_classes = 10
 batch_size = 128
-epochs_count = 240
+epochs_count = 200
 num_workers = 12
 
 parser = argparse.ArgumentParser()
@@ -21,16 +22,16 @@ args = parser.parse_args()
 datasets = cifar10.get_datasets(data_dir)
 
 if args.model_name == 'resnet':
-    model = resnet20(num_classes=num_classes)  # 0.877 - 272,474
+    model = resnet32(num_classes=num_classes)  # 0.877 - 272,474
 elif args.model_name == 'senet':
-    model = se_resnet20(num_classes=num_classes)  # 0.877 - 274,490(+0.7%)
+    model = se_resnet32(num_classes=num_classes)  # 0.877 - 274,490(+0.7%)
 elif args.model_name == 'srmnet':
-    model = srm_resnet20(num_classes=num_classes)  # and 0.871 - 273,818( +0.5%)
+    model = srm_resnet32(num_classes=num_classes)  # and 0.871 - 273,818( +0.5%)
 
 optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9,
                       weight_decay=1e-4)
 
-scheduler = StepLR(optimizer, 40, 0.2)
+scheduler = MultiStepLR(optimizer, [70, 80], 0.1)
 criterion = nn.CrossEntropyLoss()
 
 backbone = Bone(model,
