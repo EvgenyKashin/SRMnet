a9bcaa5fa037a098a9ccad158b91a73842b78463
EvgenyKashin <kashinevge@gmail.com>
2019-05-25 12:08:22 +0300

    Change scheduler

--- cifar10_train.py
+++ cifar10_train.py
@@ -1,13 +1,13 @@
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim.lr_scheduler import ReduceLROnPlateau, StepLR
+from torch.optim.lr_scheduler import StepLR
 from back import Bone, utils
 from datasets import cifar10
 from models.resnet import resnet20
 from models.custom_resnet import se_resnet20, srm_resnet20
 
 data_dir = 'cifar10'
-model_name = 'resnet'
+model_name = 'srmnet'  # resnet, senet, srmnet
 num_classes = 10
 batch_size = 128
 epochs_count = 240
@@ -24,8 +24,7 @@ elif model_name == 'srmnet':
 
 optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9,
                       weight_decay=1e-4)
-# scheduler = ReduceLROnPlateau(optimizer, 'max', factor=0.1, patience=10,
-#                               verbose=True)
+
 scheduler = StepLR(optimizer, 80, 0.1)
 criterion = nn.CrossEntropyLoss()
 
@@ -35,7 +34,7 @@ backbone = Bone(model,
                 optimizer,
                 scheduler=scheduler,
                 scheduler_after_ep=False,
-                early_stop_epoch=25,
+                # early_stop_epoch=40,
                 metric_fn=utils.accuracy_metric,
                 metric_increase=True,
                 batch_size=batch_size,
